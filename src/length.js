63802ac72107c25696c93447afa8a5f902b7a077
Luke Boyle <luke.boyle.au@gmail.com>
2017-08-29 22:33:58 +1000

    feat: add metersToFeet function

--- src/length.js
+++ src/length.js
@@ -1,9 +1,11 @@
 import inchesToMeters from './length/inches-to-meters';
 import metersToInches from './length/meters-to-inches';
 import feetToMeters from './length/feet-to-meters';
+import metersToFeet from './length/meters-to-feet';
 
 export default {
 	inchesToMeters,
 	metersToInches,
 	feetToMeters,
+	metersToFeet
 };
---
+++ src/length/meters-to-feet.js
@@ -0,0 +1,8 @@
+/**
+ * Converts Meters to Feet
+ * @param {number} meters
+ * @returns {number}
+ */
+export default function(meters) {
+	return meters * 3.28084;
+}
