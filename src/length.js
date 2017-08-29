d42e763b69507ebc9517c1eb63ded529401636a8
Luke Boyle <luke.boyle.au@gmail.com>
2017-08-29 22:33:17 +1000

    chore: import metersToInches

--- src/length.js
+++ src/length.js
@@ -1,5 +1,7 @@
 import inchesToMeters from './length/inches-to-meters';
+import metersToInches from './length/meters-to-inches';
 
 export default {
-	inchesToMeters
+	inchesToMeters,
+	metersToInches,
 };
