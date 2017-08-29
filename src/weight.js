7121dd7169b776a0b38ca25309b464cc19fe0bac
Luke Boyle <luke.boyle.au@gmail.com>
2017-08-29 22:35:24 +1000

    feat: add stonesToKilograms functions

--- src/weight.js
+++ src/weight.js
@@ -1,9 +1,11 @@
 import kilogramsToPounds from './weight/kilograms-to-pounds';
 import kilogramsToStones from './weight/kilograms-to-stones';
 import poundsToKilograms from './weight/pounds-to-kilograms';
+import stonesToKilograms from './weight/stones-to-kilograms';
 
 export default {
 	poundsToKilograms,
 	kilogramsToPounds,
 	kilogramsToStones,
+	stonesToKilograms
 };
---
+++ src/weight/stones-to-kilograms.js
@@ -0,0 +1,8 @@
+/**
+ * Convert Stones to Kilograms
+ * @param {number} stones
+ * @returns {number}
+ */
+export default function(stones) {
+    return stones * 6.35029;
+}
