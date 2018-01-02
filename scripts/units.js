221bf39ca3ec43631e3163d906f0ad54b7a05176
Luke Boyle <luke.boyle.au@gmail.com>
2018-01-03 00:17:28 +1100

    feat: add test generation to unit script

--- scripts/units.js
+++ scripts/units.js
@@ -12,7 +12,7 @@ const chalk = require('chalk');
  * @property {string} name
  * @property {Array.<{
  *     name: {string}
- *     conversionFormulae: {Object}
+ *     convertTo: {Object}
  *     functions: {Array}
  * }>} units
  */
@@ -20,20 +20,42 @@ categories.map(category => {
 	const unitsWithFunctions = category.units.map(unit => {
 		return {
 			...unit,
-			functions: Object.entries(unit.conversionFormulae).map(([key, value]) => {
+			functions: Object.entries(unit.convertTo).map(([key, value]) => {
+				const functionAsKebabCase = `${unit.name}-to-${key}`;
+				const functionName = camelCase(functionAsKebabCase);
+
 				return {
-					name: `${unit.name}-to-${key}`,
+					name: functionAsKebabCase,
 					contents: `/**
  * Converts ${titleCase(unit.name)} (${unit.symbol}) to ${titleCase(key)} (${category.units.find(unit => unit.name === key).symbol})
  * @param {number} ${unit.name}
  * @returns {number}
  */
 export default function(${unit.name}) {
-	return ${value.replace('n', unit.name)};
+	return ${value.formula.replace('n', unit.name)};
 }
+`,
+					tests: `import ${functionName} from '../../src/${category.name}/${functionAsKebabCase}';
+import expect from 'expect';
+
+test('it should exist', () => {
+
+	expect(
+		${functionName}
+	).toExist();
+
+});
+
+test('it should return the correct values', () => {
+
+	expect(
+		${functionName}(1)
+	).toBe(${value.nEqualsOne});
+
+});	
 `
 				};
-			})
+			}),
 		};
 	});
 
@@ -53,6 +75,12 @@ export default function(${unit.name}) {
 			shell.mkdir('-p', categoryPath);
 			console.log(chalk.green(`Writing ${unitFunction.name} to ${functionPath}`));
 			fs.writeFileSync(functionPath, unitFunction.contents);
+
+			const testsPath = `./tests/${category.name}`;
+
+			shell.mkdir('-p', testsPath);
+			console.log(chalk.green(`Writing ${unitFunction.name} tests to ${testsPath}`));
+			fs.writeFileSync(`${testsPath}/${unitFunction.name}.test.js`, unitFunction.tests);
 		});
 	});
 
@@ -63,7 +91,7 @@ export default {${categoryFileFunctions.reduce((acc, curr) => {
 		return `${acc}${acc !== '' ? ',' : ''}\n\t${curr}`
 	}, '')}
 };
-	`);
+`);
 
 	glob('./src/*.js', (err, files) => {
 		const filesWithoutIndex = files.filter(file => !file.includes('index.js'));
@@ -80,7 +108,7 @@ export default {${filesWithoutIndex.reduce((acc, curr) => {
 	return `${acc}${acc !== '' ? ',' : ''}\n\t...${filename}`;
 }, '')}
 };
-		`;
+`;
 		fs.writeFileSync('./src/index.js', contents);
 	});
 });
