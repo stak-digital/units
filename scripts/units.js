3b29c318cf9cf9ae928d69dffaebdc642f3c48cd
Luke Boyle <luke.boyle.au@gmail.com>
2018-01-02 17:46:42 +1100

    feat: add unit function generation script

--- scripts/units.js
+++ scripts/units.js
@@ -1,12 +1,52 @@
 const categories = require('../src/data/categories.json');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const titleCase = require('title-case');
 
 /**
  * @typedef {Object} Category
  * @property {string} name
- * @property {Array.<Object>} units
+ * @property {Array.<{
+ *     name: {string}
+ *     conversionFormulae: {Object}
+ *     functions: {Array}
+ * }>} units
  */
 categories.map(category => {
 	const categoryFileName = `${category.name}.js`;
 
-	console.log(category.units);
+	const unitsWithFunctions = category.units.map(unit => {
+		return {
+			...unit,
+			functions: Object.entries(unit.conversionFormulae).map(([key, value]) => {
+				return {
+					name: `${unit.name}-to-${key}`,
+					contents: `
+/**
+ * Converts ${unit.name} (${unit.symbol}) to ${key}
+ * @param {number} ${unit.name}
+ * @returns {number}
+ */
+export default function(${unit.name}) {
+	return ${value.replace('n', unit.name)};
+}
+						`
+				};
+			})
+		};
+	});
+
+	unitsWithFunctions.forEach(unit => {
+		unit.functions.forEach(unitFunction => {
+			console.log(`../src/${category.name}`);
+			mkdirp(`./src/${category.name}`, err => {
+				if (!err) {
+					fs.writeFileSync(`./src/${category.name}/${unitFunction.name}.js`, unitFunction.contents);
+				} else {
+					console.error(err);
+				}
+			})
+		});
+	});
 });
