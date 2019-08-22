b988cb2cd0792d6c5b808c801067ddf3ea7a41a1
Luke Boyle <me@lukeboyle.com>
2019-08-22 22:11:20 +1000

    chore: fix build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 
 import area from './area'
+import computerData from './computer-data'
+import current from './current'
+import force from './force'
+import frequency from './frequency'
+import temperature from './temperature'
+import time from './time'
+import weight from './weight'
 
 export default {
-	...area
+	...area,
+	...computerData,
+	...current,
+	...force,
+	...frequency,
+	...temperature,
+	...time,
+	...weight
 };
